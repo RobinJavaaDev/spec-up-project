5d86542a13c8e15628952d83c9f60fb73da05f3c
Art Kim <art.kim@example.com>
2025-08-19 21:52:42 +0900

    feat: 로그인 기능 구현

---
+++ src/main/java/com/supring/spec_up_project/SpecUpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.supring.spec_up_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecUpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecUpProjectApplication.class, args);
+	}
+
+}
